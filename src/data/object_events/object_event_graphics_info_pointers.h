fd8ba1e6c09a9f5f0f9a4ef7077f0c13bc24fa23
AdrienBLANCHART <adrien.blanchart@philibertnet.com>
2025-11-18 14:55:03 +0100

    fix ?

--- src/data/object_events/object_event_graphics_info_pointers.h
+++ src/data/object_events/object_event_graphics_info_pointers.h
@@ -495,7 +495,7 @@ const struct ObjectEventGraphicsInfo *const gObjectEventGraphicsInfoPointers[NUM
     [OBJ_EVENT_GFX_OW_MON] =                   &gObjectEventGraphicsInfo_Follower,
     [OBJ_EVENT_GFX_LIGHT_SPRITE] =             &gObjectEventGraphicsInfo_BallLight,
     [OBJ_EVENT_GFX_MASTER_BALL] =             &gObjectEventGraphicsInfo_MasterBall,
-    [OBJ_EVENT_GFX_SALAMENCE_MEGA_1] =        &gSpeciesInfoGen3[SPECIES_SALAMENCE].overworldData
+    [OBJ_EVENT_GFX_SALAMENCE_MEGA_1] =        &gSpeciesInfo[SPECIES_SALAMENCE].overworldData
 };
 
 const struct ObjectEventGraphicsInfo *const gMauvilleOldManGraphicsInfoPointers[] = {
