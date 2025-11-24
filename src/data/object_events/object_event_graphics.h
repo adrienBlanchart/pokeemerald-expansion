a168d3256a08543b5688984bce3fc7ceb6c4c5bd
AdrienBLANCHART <adrien.blanchart@philibertnet.com>
2025-11-24 10:22:03 +0100

    better ?

--- src/data/object_events/object_event_graphics.h
+++ src/data/object_events/object_event_graphics.h
@@ -474,11 +474,13 @@ const u16 gFieldEffectObjectPalette_CaveDust[] = INCBIN_U16("graphics/field_effe
 
 #define DEFINE_OBJ_ASSETS(name) \
 const u16 gObjectEventPal_##name##_1[] = INCBIN_U16("graphics/pokemon/" #name "/overworld_normal.gbapal"); \
-const u32 gObjectEventPic_##name##_1[] = INCBIN_COMP("graphics/pokemon/" #name "/overworld.4bpp")
+const u32 gObjectEventPic_##name##_1[] = INCBIN_COMP("graphics/pokemon/" #name "/overworld.4bpp");
+
 
 #define DEFINE_OBJ_ASSETS_MEGA(name) \
 const u16 gObjectEventPal_##name##Mega_1[] = INCBIN_U16("graphics/pokemon/" #name "/mega/overworld_normal.gbapal"); \
-const u32 gObjectEventPic_##name##Mega_1[] = INCBIN_COMP("graphics/pokemon/" #name "/mega/overworld.4bpp")
+const u32 gObjectEventPic_##name##Mega_1[] = INCBIN_COMP("graphics/pokemon/" #name "/mega/overworld.4bpp");
+
 
 
 /* 
@@ -489,6 +491,6 @@ const u16 gObjectEventPal_Bagon_1[] = INCBIN_U16("graphics/pokemon/bagon/overwor
 const u32 gObjectEventPic_Bagon_1[] = INCBIN_COMP("graphics/pokemon/bagon/overworld.4bpp");
  */
 
-DEFINE_OBJ_ASSETS(Bagon);
-DEFINE_OBJ_ASSETS_MEGA(Salamence);
-DEFINE_OBJ_ASSETS(Shuckle);
+DEFINE_OBJ_ASSETS(Bagon)
+DEFINE_OBJ_ASSETS_MEGA(Salamence)
+DEFINE_OBJ_ASSETS(Shuckle)
